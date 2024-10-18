2694a93eb6fdeaeac104f71460b1dd93a67cdb7b
Raphaël ANDRÉ <=>
2024-10-18 16:26:38 +0200

    refactor: layout in dashboards

---
+++ app/dashboard/produits/produits.tsx
@@ -0,0 +1,8 @@
+export default function Produits() {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <h1>Produits</h1>
+      </div>
+    );
+  }
+  
---
+++ app/dashboard/users/page.tsx
@@ -0,0 +1,8 @@
+export default function Users() {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        <h1>Users</h1>
+      </div>
+    );
+  }
+  
